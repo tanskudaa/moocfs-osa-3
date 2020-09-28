8bddd96ed11353b176d634fa64a812e6bfcb5750
Taneli Hongisto <taneli.hongisto@protonmail.com>
2020-09-28 15:36:24 +0300

    uibuild

---
+++ build/precache-manifest.c0ba80c583483868a5e9af07a4313884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cd55be4e8bbcb9f7507cf318aada0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0ca783722d43761c62d",
+    "url": "/static/css/main.f4337f67.chunk.css"
+  },
+  {
+    "revision": "d59aecef2aebb428629b",
+    "url": "/static/js/2.33f066a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33f066a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ca783722d43761c62d",
+    "url": "/static/js/main.c3c23151.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
